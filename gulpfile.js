8fccb70551a496d32a9d4155d28571fce0e51d91
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-03-31 09:39:19 +0900

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require("gulp");
 
 var sass = require("gulp-sass");
- 
+
+gulp.task('default', ['sass','slds-copy']);
+
 gulp.task("sass", function() {
     gulp.src("./public/stylesheets/sass/**/*scss")
         .pipe(sass())
         .pipe(gulp.dest("./public/stylesheets/css"));
+});
+
+gulp.task("slds-copy", function() {
+    return gulp.src("./node_modules/@salesforce-ux/design-system/assets/**/*")
+        .pipe(gulp.dest( './public/lib/SLDS/assets/' ) );
 });
